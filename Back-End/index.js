93129579d28f82487fffe89f0942002c74024e9a
ColineAuber <coline.auber@etud.univ-jfc.fr>
2024-01-23 17:05:59 +0100

    ok conception du serveur de monde + chacun son monde

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+
+let world = require("./world")
+
 // Construct a schema, using GraphQL schema language
-const typeDefs = require("./schema.js")
+const typeDefs = require("./schema.js");
+
 // Provide resolver functions for your schema fields
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-    },
-};
-const server = new ApolloServer({ typeDefs, resolvers})
+const resolvers = require("./resolvers")
+
+const server = new ApolloServer({
+    typeDefs,resolvers,
+    context: async ({ req }) => ({
+        world: await readUserWorld(req.headers["x-user"]),
+        user: req.headers["x-user"]
+    })
+})
 const app = express();
 app.use(express.static('public'));
 server.start().then( res => {
@@ -17,4 +24,15 @@ server.start().then( res => {
         console.log(`🚀 Server ready at
 http://localhost:4000${server.graphqlPath}`)
     );
+
+    async function readUserWorld(user) {
+        try {
+            const data = await fs.readFile("userworlds/"+ user + "world.json");
+            return JSON.parse(data);
+        }
+        catch(error) {
+            return world
+        }
+    }
+
 })
